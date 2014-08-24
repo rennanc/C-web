c6d80a060564456263bc3ed876e8218cb16633e3
rennanc <rennanchagas@hotmail.com>
2014-08-24 17:49:52 -0300

    Adição de Ajax na pagina inicial

---
+++ MVCtrab/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtrab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Feito por Rennan Chagas";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "rennanchagas@hotmail.com";
+
+            return View();
+        }
+    }
+}
