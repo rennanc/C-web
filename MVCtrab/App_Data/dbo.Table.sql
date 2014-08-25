3959254f18e552be664739d433e0adffd175145b
rennanc <rennanchagas@hotmail.com>
2014-08-25 10:48:40 -0300

    Inserção do Banco de dados no github

---
+++ MVCtrab/App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[FK_idAddress] INT NULL, 
+	[FK_idPhone] INT NULL,
+    [Name] VARCHAR(255) NULL, 
+    [Branch] VARCHAR(255) NULL, 
+    [Owner] VARCHAR(255) NULL 
+)
